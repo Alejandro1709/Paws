7c4f00ed4e8b0fd3a08297239eafc84dd2391465
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-10-01 15:43:25 -0500

    Add: SwiftData model container

--- Paws/Pet.swift
+++ Paws/Pet.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Pet {
+    var name: String
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(name: String, photo: Data? = nil) {
+        self.name = name
+        self.photo = photo
+    }
+}
