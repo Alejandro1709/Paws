83569b48c9e27c569ed8461ad369f7d11d1ba9e7
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-10-01 15:33:11 -0500

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Alejandro La Rosa on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
